7119dab2f5749da53f79385b1f7dfc327b3d2580
Typ-0 <142792234+Typ-0@users.noreply.github.com>
2024-11-19 06:00:37 -0300

    Eval branch

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 const one = document.getElementById('one');
-const two = document.getElementById('two'); 
-const three = document.getElementById('three'); 
+const two = document.getElementById('two');
+const three = document.getElementById('three');
 const four = document.getElementById('four')
 const five = document.getElementById('five')
 const six = document.getElementById('six')
@@ -11,7 +11,7 @@ const nine = document.getElementById('nine');
 const plus = document.getElementById('plus');
 const minus = document.getElementById('minus');
 const x = document.getElementById('x');
-const dot = document.getElementById('dot');
+const back = document.getElementById('back');
 const divide = document.getElementById('divide');
 const zero = document.getElementById('0');
 const equal = document.getElementById('equal');
@@ -30,9 +30,9 @@ const nineValue = '9';
 const zeroValue = '0';
 const plusValue = '+';
 const minusValue = '-';
-const xValue = 'x';
-const dotValue = '.';
-const divideValue = '÷';
+const xValue = '*';
+const backValue = '.';
+const divideValue = '/';
 const equalValue = '=';
 const clearValue = 'C';
 const displayValue = display.value;
@@ -90,49 +90,57 @@ plus.addEventListener('click', () => {
     display.value += plusValue;
     operator = plusValue;
     console.log(operator);
-   
-    
+
+
 })
 
 
 minus.addEventListener('click', () => {
     number1 = display.value;
     display.value += minusValue;
-    operator = minusValue;
-    console.log(operator);
+    
+   
 })
 
 x.addEventListener('click', () => {
     number1 = display.value;
     display.value += xValue;
-    operator = xValue;
-    console.log(operator);
+   
 })
 
-dot.addEventListener('click', () => {
-    display.value += dotValue;
+back.addEventListener('click', () => {
+    let backArrow = display.value;
+    display.value = backArrow.substring(0, backArrow.length - 1);
 })
 
 divide.addEventListener('click', () => {
     number1 = display.value;
     display.value += divideValue;
-    operator = divideValue;
-    console.log(operator);
+    
+})
+
+equal.addEventListener('click', () => {
+    function calculate() {
+        let number1 = display.value;
+        if(number1){
+            display.value = eval(number1);
+        }
+    }
+    calculate();    
 })
 
 
 
 
 
-
-function operate(operator, number1, number2) {
-    if (operator === '+') {
-        return number1 + number2;
-    } else if (operator === '-') {
-        return number1 - number2;
-    } else if (operator === 'x') {
-        return number1 * number2;
-    } else if (operator === '÷') {
-        return number1 / number2;
-    }
-}
+// function operate(operator, number1, number2) {
+//     if (operator === '+') {
+//         return number1 + number2;
+//     } else if (operator === '-') {
+//         return number1 - number2;
+//     } else if (operator === 'x') {
+//         return number1 * number2;
+//     } else if (operator === '÷') {
+//         return number1 / number2;
+//     }
+// }
